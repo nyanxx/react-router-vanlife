479c0281a4d1432d478ebfbfa298e4a4b98d1a30
Nayan Lonkar <82363466+nyanxx@users.noreply.github.com>
2026-02-06 15:12:47 +0530

    feat: same app but with react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
-import { useState, type JSX } from "react"
+import { type JSX } from "react"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 export default function App(): JSX.Element {
 
-    const [page, setPage] = useState("home")
-    // A Simple React App Without Router
+    function Home(): JSX.Element {
+        return <h1>Welcome to Home Page</h1>
+    }
+
+    function About(): JSX.Element {
+        return <h1>About Us</h1>
+    }
+
+    // Same app with React Router
     return (
-        <>
-            <div>
-                <nav>
-                    <button onClick={() => (setPage("home"))}>Home</button>
-                    <button onClick={() => (setPage("about"))}>About</button>
-                </nav>
-            </div>
-            {page === "home" && <h1>Welcome to Home Page</h1>}
-            {page === "about" && <h1>About Us</h1>}
-        </>
+        <BrowserRouter >
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+            </nav>
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
